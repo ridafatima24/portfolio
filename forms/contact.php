3ca680bcd6b58740ce61937f864673ff202fa444
ridafatima24 <ridafatima09876@gmail.com>
2025-07-04 17:28:27 +0500

    updated acording to sqa

--- forms/contact.php
+++ forms/contact.php
@@ -32,10 +32,8 @@
     'port' => '587'
   );
   */
-
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
-
   echo $contact->send();
 ?>
